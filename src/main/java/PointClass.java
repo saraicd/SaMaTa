17d39e76cc168b53bc6765d571197462c3510435
Thales Lemos <thalesflemos@gmail.com>
2022-05-02 12:06:38 +0100

    changes class PointClass

---
+++ src/main/java/PointClass.java
@@ -0,0 +1,13 @@
+package main.java;
+
+public class PointClass {
+    double coordX;
+    double coordY;
+
+
+    public PointClass (double coordX, double coordY){
+        this.coordX = coordX;
+        this.coordY = coordY;
+    }
+
+}
