6b7e43e4e3164d4191c5a82722220c43f2f486c3
MarianaFMota <marianafmota96@gmail.com>
2022-05-02 14:57:26 +0100

    Area method

--- src/main/java/Circle.java
+++ src/main/java/Circle.java
@@ -1,12 +1,16 @@
 package main.java;
 
-public class Circle extends Figure{
+public class Circle extends Figure {
     private PointClass center;
     private double radius;
 
-    public Circle (PointClass center, double radius) {
+    public Circle(PointClass center, double radius) {
         this.center = center;
         this.radius = radius;
     }
 
+    @Override
+    public double area() {
+        return Math.PI * Math.pow(this.radius, 2);
+    }
 }
--- src/main/java/Figure.java
+++ src/main/java/Figure.java
@@ -2,4 +2,6 @@ package main.java;
 
 public abstract class Figure {
     String color;
+
+    public abstract double area();
 }
