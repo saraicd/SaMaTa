f668d4570000ea4511e0d3b81d93cc09236d86ed
MarianaFMota <marianafmota96@gmail.com>
2022-05-02 14:06:38 +0100

    Class Rectangle

---
+++ src/main/java/Rectangle.java
@@ -0,0 +1,13 @@
+package main.java;
+
+import java.awt.*;
+
+public class Rectangle extends Figure{
+    private Point upperLeft;
+    private Point bottomRight;
+
+    public Rectangle(Point upperLeft, Point bottomRight) {
+        this.upperLeft = upperLeft;
+        this.bottomRight = bottomRight;
+    }
+}
