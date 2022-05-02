7a87ed21db34cc213296aa697ad77b1da566e2c8
Thales Lemos <thalesflemos@gmail.com>
2022-05-02 14:27:40 +0100

    Circle class added

---
+++ src/main/java/Circle.java
@@ -0,0 +1,12 @@
+package main.java;
+
+public class Circle extends Figure{
+    private PointClass center;
+    private double radius;
+
+    public Circle (PointClass center, double radius) {
+        this.center = center;
+        this.radius = radius;
+    }
+    
+}
